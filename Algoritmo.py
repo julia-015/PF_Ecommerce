2f99209cc1ea7e9fa7e29c019efb42d042ce0f59
TH4YSZ <thainacristina.sz07@gmail.com>
2023-09-21 14:09:22 -0300

    Cadastro cliente

--- Algoritmo.py
+++ Algoritmo.py
@@ -6,30 +6,32 @@ loja = Loja("VS STORE", "Av. das Codificações, Nº1011", 123456789)
 sair = False
 menu_adm = 0
 menu_cli = 0
-root = Adm("adm", "admin123", 1)
-admID = 0
+admin = Adm("adm", "admin123", 1)
+
+contID = 0
 
 while sair == False:
     try:
-        # os.system("cls")
+        os.system("cls")
         print(" --- BEM VINDO(A) AO VS STORE --- \n")
         print(" [1] - LOGIN ADM")
         print(" [2] - LOGIN CLIENTE")
         print(" [3] - SAIR")
-        menu = int(input("\nDigite a opção desejada: "))
+
+        menu = int(input("\nDigite a opção desejada:  "))
         os.system("cls")
         
         match menu:
             case 1:
-        
-                print("--- LOGIN ADM ---")
+                print("--- ADM LOGIN ---")
                 user_login = input("Digite o user: ")
                 senha_login = input("Digite a senha: ")
 
-                id_adm = loja.login_adm(user_login, senha_login)
-                if id_adm is not None:
-                    while menu_adm == 0: 
-                        print("--- MENU ADM --- \n")
+                if user_login == admin.user and senha_login == admin.senha:
+                    os.system("cls")
+                
+                    while menu_adm == 0:
+                        print("--- MENU DO ADM --- \n")
                         print("[1] - CADASTRAR CLIENTE")
                         print("[2] - CADASTRAR ADM")
                         print("[3] - CADASTRAR PRODUTO")
@@ -39,29 +41,50 @@ while sair == False:
                         print("[7] - LISTAR PRODUTO")
                         print("[8] - VOLTAR")
                         op_adm = int(input("\nDigite a opção desejada: "))
+
+                        match op_adm:
+                            case 1:
+                                os.system("cls")
+                                contID += 1
+                                idc = contID
+                                
+                                print("--- CADASTRO DE CLIENTE --- \nPreencha as informações:\n")
+                                nome = input("NOME: ")
+                                cpf = int(input("CPF: "))
+                                idade = int(input("IDADE: "))
+                                endereco = input("ENDEREÇO: ")
+                                senha = input("SENHA: ")
+
+                                admin.cadastrar_cliente(nome, cpf, idade, endereco, senha, idc)
+
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                pass
+                            case 6:
+                                pass
+                            case 7:
+                                pass
+                            case 8:
+                                os.system("cls")
+                                menu_adm = 1
+
+                            case _:
+                                print("Opção inválida.")
                         
                 else:
-                    print(TypeError)
+                    print("Credenciais inválidas. Tente novamente.")
             case 2:
                 os.system("cls")
                 print("--- LOGIN CLIENTE ---")
                 cpf_cliente = int(input("Digite o CPF: "))
                 senha_cliente = input("Digite a senha: ")
 
-                id_cli = root.login_cliente(user_login, senha_login)
-                if id_cli is not None:
-                    os.system("cls")
-                    print(f"Bem-vindo, {Loja.clientes[id_cli].user}!\n")
-
-                    while menu_cli == 0:                
-                        print("--- MENU CLIENTE --- \n")
-                        print("[1] - ADICIONAR PRODUTO")
-                        print("[2] - EXCLUIR PRODUTO")
-                        print("[3] - LISTAR PRODUTO")
-                        print("[4] - MEU CARRINHO")
-                        print("[5] - VOLTAR") 
-                        op_cli = int(input("\nDigite a opção desejada: "))               
-            case 3:
+            case 3:  
                 print("SAINDO...")
                 sair = True
             case _:
@@ -72,4 +95,5 @@ while sair == False:
             print("Erro:", erro.__class__.__name__)
             os.system("pause")
             os.system("cls")
+
 
--- Classes.py
+++ Classes.py
@@ -3,7 +3,8 @@ class Adm:
         self.user = user
         self.senha = senha
         self.id_adm = id_adm
-
+        self.clientes = {}
+    
     def getUser(self):
         return self.user
     
@@ -11,19 +12,20 @@ class Adm:
         return self.senha
     
     def cadastrar_adm(self, user, senha, id_adm):
-        adm = ADM(user, senha,id_adm)
+        adm = Adm(user, senha, id_adm)
         self.adm[id_adm] = adm
-        
-    def cadastrar_cliente(self, nome, idade, cpf, endereco, senha,id_cli):
-        cliente = Cliente(nome, idade, cpf, endereco, senha,id_cli)
-        self.clientes[id_cli] = cliente
+
+    def cadastrar_cliente(self, nome, cpf, idade, endereco, senha, idc):
+        cliente = Cliente(nome, cpf, idade, endereco, senha, idc)
+        self.clientes[id] = cliente
 
     def cadastrar_produto(self, nome_produto, descricao, valor):
-        produto = Produto(nome_produto, descricao, valor)
-        self.produtos[id_pdt] = produto
+        produto = produto (nome_produto, descricao, valor)
+        self.produto[a] = produto
     
     def estoque(self, produto_id, qtd):
-        pass
+        estoque = estoque (produto_id,qtd)
+        self.estoques = [a]
 
     def excluir_produto():
         pass
@@ -34,35 +36,58 @@ class Adm:
     def excluir_adm():
         pass
 
-    def listar_produto():
-        pass
+    def listar_produto(self):
+        for num, produto in self.produtos.items():
+            print(f"{num} - Nome: {produto}, Valor: R${Valor}, Descrição: {Descrição}")
     
-    def listar_cliente():
-        pass
+    def listar_cliente(self):
+        for idc, cliente in self.id.items():
+            print(f"{num} - Nome: {cliente.user}")
     
-    def listar_adm():
-        pass
+    def listar_adm(self):
+        for id_adm, adm in self.adm.items(id_adm):
+            print(f"{num} - Nome: {adm.user}")
 
 class Cliente:
-    def __init__(self, nome, idade, cpf, endereco, senha):
+    def __init__(self, nome, cpf, idade, endereço, senha, idc):
         self.nome = nome 
         self.idade = idade
         self.cpf = cpf
-        self.endereco = endereco
+        self.endereco = endereço
         self.senha = senha
+        self.id = idc
 
-    def adicionar_produto():
-        pass
-    def excluir_produto():
-        pass
-    def meu_carrinho():
-        pass
+    def adicionar_produto(self, idc, produto_id, quantidade):
+        if idc in self.carrinhos:
+                    carrinho = self.carrinhos[idc]
+                    if produto_id in self.produtos:
+                        produto = self.produtos[produto_id]
+                        carrinho.adicionar_produto(produto_id, quantidade)
+                    else:
+                        print("Produto não encontrado.")
+        else:
+            print("Carrinho não encontrado.")
+            
+    def excluir_produto(self, idc, produto_id, quantidade):
+        if idc in self.carrinhos:
+            carrinho = self.carrinhos[idc]
+            if produto_id in self.produtos:
+                produto = self.produtos[produto_id]
+                carrinho.remover_produto(produto_id, quantidade)
+    def meu_carrinho(self, idc):
+        if idc in self.carrinhos:
+            carrinho = self.carrinhos[idc]
+            print("Produtos no carrinho:")
+            for produto_num, quantidade in carrinho.produtos.items():
+                produto = self.produtos[produto_num]
+                print(f"Nome: {nome_produto}, Quantidade: {quantidade}")
 
 class Produto:
-    def __init__(self, nome_produto, descricao, valor):
+    def __init__(self, nome_produto, descricao, valor, quantidade):
         self.nome_produto = nome_produto
         self.descricao = descricao
         self.valor = valor
+        self.quantidade = quantidade
 
 class Carrinho:
     def __init__(self, nome, valor, descricao):
@@ -76,18 +101,11 @@ class Loja:
         self.endereco = endereco
         self.cnpj = cnpj
         self.adm = {}
-        self.clientes = {}
         self.produtos = {}
+        
     
     def login_cliente(self, cpf, senha):
         for id, cliente in self.clientes.items():
             if cpf == cliente.cpf and senha == cliente.senha:
                 return id
-        return None
-    
-    def login_adm(self, user_login, senha_login):
-        
-        for id_adm, adm in self.adm.items():
-            if user_login == adm.user and senha_login == adm.senha:
-                return id_adm
         return None
