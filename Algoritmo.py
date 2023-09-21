b185e683205fef5d92572a89e6bcb4eaf3d89323
TH4YSZ <thainacristina.sz07@gmail.com>
2023-09-21 09:38:18 -0300

    Criação das classes

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,1 +1,40 @@
+from Classes import *
 
+import os
+
+sair = False
+while sair == False:
+    try:
+        os.system("cls")
+        print(" --- BEM VINDO(A) AO  --- ")
+        print(" [1] - LOGIN ADM")
+        print(" [2] - LOGIN CLIENTE")
+        print(" [3] - SAIR")
+        menu = int(input("Digite a opção desejada >> "))
+        
+        match menu:
+            case 1:
+                
+                print("[1] - CADASTRAR CLIENTE")
+                print("[2] - CADASTRAR ADM")
+                print("[3] - CADASTRAR PRODUTO")
+                print("[4] - EXCLUIR PRODUTO")
+                print("[5] - EXCLUIR CLIENTE")                     
+                print("[6] - LISTAR CLIENTE")
+                print("[7] - LISTAR PRODUTO")
+            case 2:
+                print("[1] - ADICIONAR PRODUTO")
+                print("[2] - EXCLUIR PRODUTO")
+                print("[3] - LISTAR PRODUTO")
+                print("[4] - VER CARRINHO")                
+            case 3:
+                print("SAINDO...")
+                sair = True
+            case _:
+                print("Opção inválida.")
+                
+    except Exception as erro:
+            print("Opção inválida.")
+            print("Erro:", erro.__class__.__name__)
+            os.system("pause")
+            os.system("cls")
--- Classes.py
+++ Classes.py
@@ -1,1 +1,66 @@
+class ADM:
+    def __init__(self, user, senha):
+        self.user = user
+        self.senha = senha
+    
+    def cadastrar_adm(self, user, senha):
+        pass
+        
+    def cadastrar_cliente(self, id, nome, idade, cpf, endereco, senha):
+        cliente = Cliente(id, nome, idade, cpf, endereco, senha)
+        self.clientes[id] = cliente
 
+    def cadastrar_produto(self, nome_produto, descricao, valor):
+        produto = Produto(nome_produto, descricao, valor)
+        self.produtos[id_pdt] = produto
+    
+    def estoque(self, produto_id, qtd):
+        pass
+
+    def excluir_produto():
+        pass
+    def excluir_cliente():
+        pass
+    def excluir_adm():
+        pass
+    def listar_produto():
+        pass
+    def listar_cliente():
+        pass
+    def listar_adm():
+        pass
+
+class Cliente:
+    def __init__(self, nome, idade, cpf, endereco, senha):
+        self.nome = nome 
+        self.idade = idade
+        self.cpf = cpf
+        self.endereco = endereco
+        self.senha = senha
+
+    def adicionar_produto():
+        pass
+    def excluir_produto():
+        pass
+
+class Produto:
+    def __init__(self, nome_produto, descricao, valor):
+        self.nome_produto = nome_produto
+        self.descricao = descricao
+        self.valor = valor
+
+class Carrinho:
+    def __init__(self, nome, valor, descricao):
+        self.nome = nome
+        self.valor = valor
+        self.descricao = descricao
+        
+class Loja:
+    def __init__(self, nome, endereco, cnpj):
+        self.nome = nome 
+        self.endereco = endereco
+        self.cnpj = cnpj
+        self.adm = {}
+        self.clientes = {}
+        self.produtos = {}
+        
